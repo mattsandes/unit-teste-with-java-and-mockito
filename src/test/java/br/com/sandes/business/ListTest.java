22afd2fd605067cc9400c053e631a5c5beaa3c53
Mateus Sandes <mateussandes@hotmail.com.br>
2024-09-11 07:51:11 -0300

    Criando um teste para mockar listas

---
+++ src/test/java/br/com/sandes/business/ListTest.java
@@ -0,0 +1,83 @@
+package br.com.sandes.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturn10(){
+        //test padrão
+
+        //Given (Arrange)
+        var list = mock(List.class);
+
+        //When (Act)
+        when(list.size()).thenReturn(10);
+
+        //Then (Assert)
+        assertEquals(10, list.size());
+
+        //realizando um retorno diferente para cada chamada;
+
+    }
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturnMultipleValues(){
+        //test padrão
+
+        //Given (Arrange)
+        var list = mock(List.class);
+
+        //When (Act)
+        when(list.size())
+                .thenReturn(10)
+                .thenReturn(20)
+                .thenReturn(20);
+
+        //Then (Assert)
+        assertEquals(10, list.size());
+        assertEquals(20, list.size());
+        assertEquals(20, list.size());
+    }
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturnSandes(){
+
+        //Given (Arrange)
+        var list = mock(List.class);
+
+        //When (Act)
+        when(list.get(0)).thenReturn("Sandes");
+
+        //Then (Assert)
+        assertEquals("Sandes", list.get(0));
+
+        //Essa assertion falha por conta que esse comportamento não foi
+        //mockada
+        //assertEquals("Sandes", list.get(15));
+    }
+
+    @Test
+    void testMockingList_When_SizeIsCalledWithArgumentMatcher_ShouldReturnSandes(){
+
+        //Given (Arrange)
+        var list = mock(List.class);
+
+        //When (Act)
+        when(list.get(anyInt())).thenReturn("Sandes"); //não vai retornar nullo, independente do que for passado aqui;
+
+        //Then (Assert)
+        assertEquals("Sandes", list.get(anyInt())); //mesma coias aqui
+
+        //Essa assertion falha por conta que esse comportamento não foi
+        //mockada
+        //assertEquals("Sandes", list.get(15));
+    }
+}
