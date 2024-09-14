5801b1a82641136f63365e4e67073ca7c0daa3e4
Mateus Sandes <mateussandes@hotmail.com.br>
2024-09-14 16:42:20 -0300

    Editada a classe de teste para usar o BDD

--- src/test/java/br/com/sandes/business/ListTest.java
+++ src/test/java/br/com/sandes/business/ListTest.java
@@ -1,16 +1,28 @@
 package br.com.sandes.business;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ListTest {
+
+    List<?> list;
+    int integer = 0;
+
+    @BeforeEach
+    void setUp(){
+        list = mock(List.class);
+    }
 
     @Test
     void testMockingList_When_SizeIsCalled_ShouldReturn10(){
@@ -23,18 +35,13 @@ public class ListTest {
         when(list.size()).thenReturn(10);
 
         //Then (Assert)
-        assertEquals(10, list.size());
-
-        //realizando um retorno diferente para cada chamada;
-
+        assertThat(list.size(), is(10));
     }
 
     @Test
     void testMockingList_When_SizeIsCalled_ShouldReturnMultipleValues(){
-        //test padrão
-
         //Given (Arrange)
-        var list = mock(List.class);
+        given(list.size()).willReturn(integer);
 
         //When (Act)
         when(list.size())
@@ -43,9 +50,9 @@ public class ListTest {
                 .thenReturn(20);
 
         //Then (Assert)
-        assertEquals(10, list.size());
-        assertEquals(20, list.size());
-        assertEquals(20, list.size());
+        assertThat(list.size(), is(10));
+        assertThat(list.size(), is(20));
+        assertThat(list.size(), is(20));
     }
 
     @Test
@@ -58,11 +65,11 @@ public class ListTest {
         when(list.get(0)).thenReturn("Sandes");
 
         //Then (Assert)
-        assertEquals("Sandes", list.get(0));
+        assertThat(list.get(0), is("Sandes"));
+        assertNull(list.get(1));
 
         //Essa assertion falha por conta que esse comportamento não foi
         //mockada
-        //assertEquals("Sandes", list.get(15));
     }
 
     @Test
@@ -76,10 +83,6 @@ public class ListTest {
 
         //Then (Assert)
         assertEquals("Sandes", list.get(anyInt())); //mesma coias aqui
-
-        //Essa assertion falha por conta que esse comportamento não foi
-        //mockada
-        //assertEquals("Sandes", list.get(15));
     }
 
     //teste para mockar exceptions com Mockito;
@@ -87,11 +90,7 @@ public class ListTest {
     void testMockingList_When_ThrowsAnException(){
 
         //Given (Arrange)
-        var list = mock(List.class);
-
-        //When (Act)
-        when(list.get(anyInt()))
-                .thenThrow(new RuntimeException("Foo Bar")); //não vai retornar nullo, independente do que for passado aqui;
+        given(list.get(anyInt())).willThrow(new RuntimeException());
 
         //Then (Assert)
         assertThrows(RuntimeException.class,
